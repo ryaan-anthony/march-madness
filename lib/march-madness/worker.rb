e5a97129a4e6890018fde0b426b0e11001d2d758
ryan tulino <rtulino@weblinc.com>
2019-03-06 23:29:44 -0500

    delegate

--- lib/march-madness/worker.rb
+++ lib/march-madness/worker.rb
@@ -1,11 +1,9 @@
 module MarchMadness
   class Worker
+    delegate :puts, to: :@output
+
     def initialize(output = $stdout)
       @output = output
-    end
-
-    def puts(message)
-      @output.puts(message)
     end
   end
 end
